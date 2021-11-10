661a774d7569f961dc0471ab2ab059229b1628fa
Damian <damianj25@gmail.com>
2021-11-10 02:11:18 +0100

    fix endpoints

--- src/main/java/com/dejot/bookstore/loans/LoansRepository.java
+++ src/main/java/com/dejot/bookstore/loans/LoansRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 public interface LoansRepository extends CrudRepository<Loans, Long> {
     Loans findByBookAndClient(Book book, Client client);
     List<Loans> findAllByClientId(Long clientId);
-
     Loans findByBookIdAndClientId(Long bookId, Long clientId);
 }
